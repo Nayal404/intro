d2566c5982425cb687013a10195bee829a636be4
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2022-01-14 15:59:49 +0545

    Add files via upload

---
+++ index_main.js
@@ -0,0 +1,15 @@
+function home(){
+    window.open('https://thisissolutiontime.blogspot.com/')
+}
+function blogs(){
+    window.open('https://thisissolutiontime.blogspot.com/')
+}
+function gt(){
+    window.open('https://github.com/Nayal404/')
+}
+function fb(){
+    window.open('https://www.facebook.com/This.is.my.email.dont.steal.mf/')
+}
+function yt(){
+    window.open('https://www.youtube.com/channel/UCTqToT5faGKMvL4X2a3Zhzg')
+}
